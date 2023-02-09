4841ae42471d5d44c56187de1ec26a0f35418667
Marcos <vinicius-mvss@live.com>
2023-02-09 19:06:10 -0300

    Implementei o Scanner no codigo

--- Advinhe-O-Numero/src/jogoAdvinhe.java
+++ Advinhe-O-Numero/src/jogoAdvinhe.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class jogoAdvinhe {
-    
+    public static void main(String[] args){
+        Scanner tentativa = new Scanner(System.in);
+        int adivinhe = 0;
+        
+        System.out.println("Tente adivinhar um número de 1 a 100");
+
+        System.out.println("Digite seu número: ");
+        adivinhe = tentativa.nextInt();
+        System.out.println(adivinhe);
+    }
 }
