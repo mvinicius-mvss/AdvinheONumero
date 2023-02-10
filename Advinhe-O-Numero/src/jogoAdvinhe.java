8016f1e84a22d0958445c3ca88aae70c053fcfcd
Marcos <vinicius-mvss@live.com>
2023-02-10 00:50:02 -0300

    Coloquei a condição while e funcionou bem.

--- Advinhe-O-Numero/src/jogoAdvinhe.java
+++ Advinhe-O-Numero/src/jogoAdvinhe.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class jogoAdvinhe {
     public static void main(String[] args){
-        Scanner tentativa = new Scanner(System.in);
-        int adivinhe = 0;
+        Scanner palpite = new Scanner(System.in);
+        Random numero = new Random();
+
+        int numeroParaAdivinhar = numero.nextInt(100) + 1;
+        int numeroDeTentativas = 0;
+        int tentativa = 0;
         
         System.out.println("Tente adivinhar um número de 1 a 100");
 
-        System.out.println("Digite seu número: ");
-        adivinhe = tentativa.nextInt();
-        System.out.println(adivinhe);
+        while(tentativa != numeroParaAdivinhar && numeroDeTentativas <7){
+
+            System.out.println("Digite seu número: ");
+            tentativa = palpite.nextInt();
+            numeroDeTentativas++;
+
+            if (tentativa < numeroParaAdivinhar)
+                System.out.println("Poxa, o número que eu quero é maior");
+            else if (tentativa > numeroParaAdivinhar)
+                System.out.println("Poxa, o número que eu quero é menor");
+        }
     }
 }
