82378eb8f3bda68e5a7ffa1e4d66bb623a745fcc
Marcos Vinicius <vinicius-mvss@live.com>
2023-02-17 16:50:32 -0300

    uma correção de escrita "palpite"

--- Advinhe-O-Numero/src/jogoAdvinhe.java
+++ Advinhe-O-Numero/src/jogoAdvinhe.java
@@ -3,29 +3,30 @@ import java.util.Random;
 
 public class jogoAdvinhe {
     public static void main(String[] args){
-        Scanner palpite = new Scanner(System.in);
-        Random numero = new Random();
+        try (Scanner palpite = new Scanner(System.in)) {
+            Random numero = new Random();
 
-        int numeroParaAdivinhar = numero.nextInt(100) + 1;
-        int numeroDeTentativas = 0;
-        int tentativa = 0;
-        
-        System.out.println("Tente adivinhar um número de 1 a 100");
+            int numeroParaAdivinhar = numero.nextInt(100) + 1;
+            int numeroDeTentativas = 0;
+            int tentativa = 0;
+            
+            System.out.println("Tente adivinhar um número de 1 a 100");
 
-        while(tentativa != numeroParaAdivinhar && numeroDeTentativas <7){
+            while(tentativa != numeroParaAdivinhar && numeroDeTentativas <7){
 
-            System.out.println("Digite seu número: ");
-            tentativa = palpite.nextInt();
-            numeroDeTentativas++;
+                System.out.println("Digite seu número: ");
+                tentativa = palpite.nextInt();
+                numeroDeTentativas++;
 
-            if (tentativa < numeroParaAdivinhar)
-                System.out.println("Poxa, o número que eu quero é maior");
-            else if (tentativa > numeroParaAdivinhar)
-                System.out.println("Poxa, o número que eu quero é menor");
+                if (tentativa < numeroParaAdivinhar)
+                    System.out.println("Poxa, o número que eu quero é maior");
+                else if (tentativa > numeroParaAdivinhar)
+                    System.out.println("Poxa, o número que eu quero é menor");
+            }
+            if (tentativa == numeroParaAdivinhar)
+                System.out.println("Parabens, você acertou o número em " + numeroDeTentativas + " tentativas.");
+            else
+                System.out.println("Infelizmente você não conseguiu, o número era: " + numeroParaAdivinhar);
         }
-        if (tentativa == numeroParaAdivinhar)
-            System.out.println("Parabens, você acertou o número em " + numeroDeTentativas + " tentativas.");
-        else
-            System.out.println("Infelizmente você não conseguiu, o número era: " + numeroParaAdivinhar);
     }
 }
